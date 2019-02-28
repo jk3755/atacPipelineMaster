41185a2a722179b0c8d78f3a252e44ae121d812b
Jordan <jsk336@gmail.com>
2019-02-28 01:06:56 -0500

    Updates

--- snakeATACCNV.R
+++ snakeATACCNV.R
@@ -4,11 +4,9 @@
 #source("https://bioconductor.org/biocLite.R")
 #biocLite("GenomicRanges", suppressUpdates = TRUE)
 #biocLite("BSgenome.Hsapiens.UCSC.hg38", suppressUpdates = TRUE)
-#biocLite("Rsubread", suppressUpdates = TRUE)
 #biocLite("genomation", suppressUpdates = TRUE)
 library(GenomicRanges)
 library(BSgenome.Hsapiens.UCSC.hg38)
-library(Rsubread)
 library(genomation)
 library(Rsamtools)
 
@@ -31,35 +29,22 @@ hg38Windows <- unlist(hg38Windows)
 ### testing
 bamFile <- "/home/ubuntu2/atac/ls1034/wt01/preprocessing/12all/LS1034-WT-01.all.bam"
 
-## Rsubread required that you add a metadata column 'id' to the GRanges object
-hg38Windows@elementMetadata$id <- c(1:length(hg38Windows@ranges@start))
-
-## Create the annotation object
-annotHg38Windows <- createAnnotationFile(hg38Windows)
-
-# Calculate the number of reads
-countHg38Windows <- featureCounts(files = bamFile, nthreads = 20, annot.ext = annotHg38Windows, isPairedEnd = TRUE)
-
-
 ## with Rsamtools
-
-#
 quickBamFlagSummary(bamFile)
-
+#
 params <- ScanBamParam(which = hg38Windows)
 aln <- countBam(bamFile, param = params)
 
 
-
-
 ## Load the called peaks as a GRanges object
-bedFile <- "C:\\Users\\Jordan\\Documents\\atac\\SNU61-WT-01.all_peaks.narrowPeak"
-snu61Peaks <- readBed(bedFile, track.line = FALSE, remove.unusual = FALSE, zero.based = TRUE)
+bedFile <- "/home/ubuntu2/atac/ls1034/wt01/preprocessing/13allpeaks/LS1034-WT-01.all_peaks.narrowPeak"
+ls1034Peaks <- readBed(bedFile, track.line = FALSE, remove.unusual = FALSE, zero.based = TRUE)
 ## Remove the entries that are not on standard chromosomes (chr1-22, X, Y), often helps prevent problems downstream
-snu61Peaks <- keepStandardChromosomes(snu61Peaks, pruning.mode="coarse")
-
+ls1034Peaks <- keepStandardChromosomes(ls1034Peaks, pruning.mode="coarse")
 
 ## Calculate the total number of reads in each peak
+params <- ScanBamParam(which = ls1034Peaks)
+alnpeaks <- countBam(bamFile, param = params)
 
 
 ## For each window, find which peaks overlap with it
