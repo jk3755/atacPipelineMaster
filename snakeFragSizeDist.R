aa56773e5f4b4851b513ea29ba40128494a18b11
Jordan <jsk336@gmail.com>
2019-03-18 21:50:55 -0400

    Added

--- snakeFragSizeDist.R
+++ snakeFragSizeDist.R
@@ -1,11 +1,27 @@
-library(ATACseqQC)
+cat("Loading libraries...", "\n")
+#BiocManager::install("ATACseqQC")
+suppressMessages(library(ATACseqQC))
 
 ##
-bamfile <- "C:\\Users\\jsk33\\Documents\\atac\\mydata\\snu61\\wt01\\bam\\SNU61-WT-01-REP1.u.bam"
-baifile <- "C:\\Users\\jsk33\\Documents\\atac\\mydata\\snu61\\wt01\\bam\\SNU61-WT-01-REP1.u.bai"
+cat("Setting snakemake vars...", "\n")
+bamPath <- snakemake@input[[1]]
+baiPath <- snakemake@input[[2]]
+outputDir <- snakemake@output[[1]]
+sampleName <- snakemake@wildcards[["mergedsample"]]
 
-test <- fragSizeDist(
-  bamFiles = bamfile,
-  bamFiles.labels = "snu61",
-  index = baifile
+
+
+##
+bamFile <- "C:\\Users\\jsk33\\Documents\\atac\\mydata\\h508\\wt02a\\H508A-WT-02-REP3.u.bam"
+baiFile <- "C:\\Users\\jsk33\\Documents\\atac\\mydata\\h508\\wt02a\\H508A-WT-02-REP3.u.bai"
+##
+svg_path <- "C:\\Users\\jsk33\\Documents\\atac\\mydata\\h508\\wt02a\\H508A-WT-02-REP3.u.fragsizes.svg"
+svg(file = svg_path) # set the filepath for saving the svg figure
+##
+fragSizeDist(
+  bamFiles = bamFile,
+  bamFiles.labels = "H508A-WT-02-REP3",
+  index = baiFile
 )
+
+dev.off()
