169df2e24b1c87bbcedc790cf76b9924cc2dde9c
Jordan <jsk336@gmail.com>
2019-03-29 16:22:36 -0400

    Writing new version of motif scanning sites scripts

---
+++ scanPWM/scanPWM.R
@@ -0,0 +1,158 @@
+> View(motifData)
+> View(motifData)
+> for (gene in uniqueGenes){
+  +   
+    +   ## Get relevant indices
+    +   geneIdx <- which(mdbHuman@elementMetadata@listData[["geneSymbol"]] == gene)
+    +   ## Number of redundant motifs for current gene
+      +   numMotifs <- length(geneIdx)
+      +   
+        +   ## Get the unique motifs for current gene
+        +   tempMotifs <- list()
+        +   c <- 1
+        +   for (idx in geneIdx){
+          +     tempMotifs[c] <- mdbHuman@listData[idx]
+          +     c <- c+1
+          +   } # end for (idx in geneIdx)
+        +   ##
+          +   uniqueMotifs <- unique(tempMotifs)
+          +   numUniqueMotifs <- length(uniqueMotifs)
+          +   
+            +   ## Initiate sublists for current gene
+            +   tryCatch({
+              +     com <- paste0("motifData$", gene, " <- list()")
+              +     eval(parse(text = com))},
+              +   error=function(cond){
+                +     message(cond)
+                +     return(NA)},
+              +   finally={}) #end tryCatch()
+          +   
+            +   ## Populate motifData list for current gene
+            +   tryCatch({
+              +     
+                +     for (a in 1:numUniqueMotifs){
+                  +       com <- paste0("motifData$", gene, "$motif", a, " <- uniqueMotifs[[a]]")
+                  +       eval(parse(text = com))
+                  +     } # end for (a in 1:numUniqueMotifs)
+              +     },
+              +     error=function(cond){
+                +       message(cond)
+                +       return(NA)},
+              +     finally={}) #end tryCatch()
+          +   
+            + } # end for (gene in uniqueGenes)
+
+
+> View(motifData)
+> View(motifData)
+> ## Extract relevant data
+  > for (gene in uniqueGenes){
+    +   
+      +   ## Get relevant indices
+      +   geneIdx <- which(mdbHuman@elementMetadata@listData[["geneSymbol"]] == gene)
+      +   ## Number of redundant motifs for current gene
+        +   numMotifs <- length(geneIdx)
+        +   
+          +   ## Get the unique motifs for current gene
+          +   tempMotifs <- list()
+          +   c <- 1
+          +   for (idx in geneIdx){
+            +     tempMotifs[c] <- mdbHuman@listData[idx]
+            +     c <- c+1
+            +   } # end for (idx in geneIdx)
+          +   ##
+            +   uniqueMotifs <- unique(tempMotifs)
+            +   numUniqueMotifs <- length(uniqueMotifs)
+            +   
+              +   ## Initiate sublists for current gene
+              +   tryCatch({
+                +     com <- paste0("motifData$", gene, " <- list()")
+                +     eval(parse(text = com))},
+                +   error=function(cond){
+                  +     message(cond)
+                  +     return(NA)},
+                +   finally={}) #end tryCatch()
+            +   
+              +   ## Populate motifData list for current gene
+              +   tryCatch({
+                +     
+                  +     for (a in 1:numUniqueMotifs){
+                    +       com <- paste0("motifData$", gene, "$motif", a, " <- list()")
+                    +       eval(parse(text = com))
+                    +     } # end for (a in 1:numUniqueMotifs)
+                +     },
+                +     error=function(cond){
+                  +       message(cond)
+                  +       return(NA)},
+                +     finally={}) #end tryCatch()
+            +   
+              + } # end for (gene in uniqueGenes)
+
+
+> ## Extract relevant data
+  > for (gene in uniqueGenes){
+    +   
+      +   ## Get relevant indices
+      +   geneIdx <- which(mdbHuman@elementMetadata@listData[["geneSymbol"]] == gene)
+      +   ## Number of redundant motifs for current gene
+        +   numMotifs <- length(geneIdx)
+        +   
+          +   ## Get the unique motifs for current gene
+          +   tempMotifs <- list()
+          +   c <- 1
+          +   for (idx in geneIdx){
+            +     tempMotifs[c] <- mdbHuman@listData[idx]
+            +     c <- c+1
+            +   } # end for (idx in geneIdx)
+          +   ##
+            +   uniqueMotifs <- unique(tempMotifs)
+            +   numUniqueMotifs <- length(uniqueMotifs)
+            +   
+              +   ## Initiate sublists for current gene
+              +   f <- tryCatch({
+                +     com <- paste0("motifData$", gene, " <- list()")
+                +     eval(parse(text = com))
+                +   },
+                +   error=function(cond){
+                  +     message(cond)
+                  +     return(NA)
+                  +   },
+                +   finally={}) #end tryCatch()
+              +   
+                +   # ## Populate motifData list for current gene
+                +   # for (a in 1:numUniqueMotifs){
+                +   #   
+                +   #   ##
+                +   #   com <- paste0("motifData$", gene, "$motif", a, " <- list()")
+                +   #   eval(parse(text = com))
+                +     
+                +  # } # end for (a in 1:numMotifs)
+                + } # end for (gene in uniqueGenes)
+
+
+> uniqueMotifs <- unique(tempMotifs)
+> View(uniqueMotifs)
+> View(uniqueMotifs)
+> numUniqueMotifs <- length(uniqueMotifs)
+> ## Query the database to return all human annotations
+  > mdbHuman <- query (MotifDb, 'hsapiens')
+  > ## Get the list of all unique genes
+    > uniqueGenes <- unique(mdbHuman@elementMetadata@listData[["geneSymbol"]])
+    > ## Get the total number of unique annotated genes
+      > numGenes <- length(uniqueGenes)
+      > ## Initiate list object to store all motif data
+        > motifData <- list()
+        
+        
+        ## Query the database to return all human annotations
+        > mdbHuman <- query (MotifDb, 'hsapiens')
+        > 
+          > ## Get the list of all unique genes
+          > uniqueGenes <- unique(mdbHuman@elementMetadata@listData[["geneSymbol"]])
+          > 
+            > ##
+            > numGenes <- length(uniqueGenes)
+            > 
+              > ## Initiate list object to store all motif data
+              > motifData <- list()
+              > 
