f1b1acc8e420d22d4dc12316080a83843594eaea
Jordan <jsk336@gmail.com>
2019-04-24 16:36:31 -0400

    Updates to code

--- panTF/scanPWM/snakeScanFIREPWM.R
+++ panTF/scanPWM/snakeScanFIREPWM.R
@@ -11,6 +11,44 @@ suppressMessages(library(BSgenome.Hsapiens.UCSC.hg38))
 suppressMessages(library(Biostrings))
 suppressMessages(library(MotifDb))
 
+
+##
+load("C:\\Users\\jsk33\\Documents\\git\\atacPipelineMaster\\panTF\\scanPWM\\coad_mr_fire_motifs.RData")
+
+
+#### PFM ####
+## ADNP
+ADNP <- list()
+ADNP$"motif1" <- COAD_FIRE_PFM[["ADNP_motif1_fire_PFM"]]
+## TAF4
+TAF4 <- list()
+TAF4$"motif1" <- COAD_FIRE_PFM[["TAF4_motif1_fire_PFM"]]
+TAF4$"motif2" <- COAD_FIRE_PFM[["TAF4_motif2_fire_PFM"]]
+## ZMYND8
+ZMYND8 <- list()
+ZMYND8$"motif1" <- COAD_FIRE_PFM[["ZMYND8_motif1_fire_PFM"]]
+## ZNF696
+ZNF696 <- list()
+ZNF696$"motif1" <- COAD_FIRE_PFM[["ZNF696_motif1_fire_PFM"]]
+ZNF696$"motif2" <- COAD_FIRE_PFM[["ZNF696_motif2_fire_PFM"]]
+ZNF696$"motif3" <- COAD_FIRE_PFM[["ZNF696_motif3_fire_PFM"]]
+
+#### PWM ####
+ADNP <- list()
+ADNP$"motif1" <- COAD_FIRE_PWM[["ADNP_motif1_fire_PWM"]]
+## TAF4
+TAF4 <- list()
+TAF4$"motif1" <- COAD_FIRE_PWM[["TAF4_motif1_fire_PWM"]]
+TAF4$"motif2" <- COAD_FIRE_PWM[["TAF4_motif2_fire_PWM"]]
+## ZMYND8
+ZMYND8 <- list()
+ZMYND8$"motif1" <- COAD_FIRE_PWM[["ZMYND8_motif1_fire_PWM"]]
+## ZNF696
+ZNF696 <- list()
+ZNF696$"motif1" <- COAD_FIRE_PWM[["ZNF696_motif1_fire_PWM"]]
+ZNF696$"motif2" <- COAD_FIRE_PWM[["ZNF696_motif2_fire_PWM"]]
+ZNF696$"motif3" <- COAD_FIRE_PWM[["ZNF696_motif3_fire_PWM"]]
+
 ##
 cat("Setting snakemake vars...", "\n")
 motifDataPath <- snakemake@input[[1]]
