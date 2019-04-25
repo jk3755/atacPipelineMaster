c8cfa9cc9c79f0889a8935bee611c137371bc63d
Jordan <jsk336@gmail.com>
2019-04-25 09:10:28 -0400

    Updates

--- panTF/snakeAggregateProcessedFootprintData.R
+++ panTF/snakeAggregateProcessedFootprintData.R
@@ -18,14 +18,22 @@ cat("Found", numFiles, "footprint data files. Processing...", "\n")
 
 ## Initialize a data frame that will store the aggregated data for all TFs
 ## Will make plot generation much easier
-aggregateFootprintData <- data.frame(matrix(vector(), 0, 23,
+aggregateFootprintData <- data.frame(matrix(vector(), 0, 53,
                           dimnames=list(c(), c(
-                          "Gene", "numMotifs", "numPeakSites", "numBoundSites", "numUnboundSites", "boundRatio",
+                          "Gene", "numMotifs", "numPeakSites", "numBoundSites", "numUnboundSites",
                           "peakMotifSignal", "peakFlankSignal", "peakBackgroundSignal", "peak.log2Flank", "peak.log2Depth",
                           "boundMotifSignal", "boundFlankSignal", "boundBackgroundSignal", "bound.log2Flank", "bound.log2Depth",
                           "unboundMotifSignal", "unboundFlankSignal", "unboundBackgroundSignal", "unbound.log2Flank", "unbound.log2Depth",
+                          "promoterPeakMotifSignal", "promoterPeakFlankSignal", "promoterPeakBackgroundSignal", "promoterPeak.log2Flank", "promoterPeak.log2Depth",
+                          "distalPeakMotifSignal", "distalPeakFlankSignal", "distalPeakBackgroundSignal", "distalPeak.log2Flank", "distalPeak.log2Depth",
+                          "promoterBoundMotifSignal", "promoterBoundFlankSignal", "promoterBoundBackgroundSignal", "promoterBound.log2Flank", "promoterBound.log2Depth",
+                          "promoterUnboundMotifSignal", "promoterUnboundFlankSignal", "promoterUnboundBackgroundSignal", "promoterUnbound.log2Flank", "promoterUnbound.log2Depth",
+                          "distalBoundMotifSignal", "distalBoundFlankSignal", "distalBoundBackgroundSignal", "distalBound.log2Flank", "distalBound.log2Depth",
+                          "distalUnboundMotifSignal", "distalUnboundFlankSignal", "distalUnboundBackgroundSignal", "distalUnbound.log2Flank", "distalUnbound.log2Depth",
+                          "boundRatio",
                           "RNAexp", "viperNES"))),
                           stringsAsFactors=F)
+
 
 
 ## Also initialize a list to store the Footprint metrics and site data for each gene
@@ -70,7 +78,39 @@ for (a in 1:numFiles){
   aggregateFootprintData[dataIdx,18] <- processedFootprintData[["unboundBackgroundSignal"]]
   aggregateFootprintData[dataIdx,19] <- processedFootprintData[["unbound.log2Flank"]]
   aggregateFootprintData[dataIdx,20] <- processedFootprintData[["unbound.log2Depth"]]
-  
+  aggregateFootprintData[dataIdx,21] <- processedFootprintData[["promoterPeakMotifSignal"]]
+  aggregateFootprintData[dataIdx,22] <- processedFootprintData[["promoterPeakFlankSignal"]]
+  aggregateFootprintData[dataIdx,23] <- processedFootprintData[["promoterPeakBackgroundSignal"]]
+  aggregateFootprintData[dataIdx,24] <- processedFootprintData[["promoterPeak.log2Flank"]]
+  aggregateFootprintData[dataIdx,25] <- processedFootprintData[["promoterPeak.log2Depth"]]
+  aggregateFootprintData[dataIdx,26] <- processedFootprintData[["distalPeakMotifSignal"]]
+  aggregateFootprintData[dataIdx,27] <- processedFootprintData[["distalPeakFlankSignal"]]
+  aggregateFootprintData[dataIdx,28] <- processedFootprintData[["distalPeakBackgroundSignal"]]
+  aggregateFootprintData[dataIdx,29] <- processedFootprintData[["distalPeak.log2Flank"]]
+  aggregateFootprintData[dataIdx,30] <- processedFootprintData[["distalPeak.log2Depth"]]
+  aggregateFootprintData[dataIdx,31] <- processedFootprintData[["promoterBoundMotifSignal"]]
+  aggregateFootprintData[dataIdx,32] <- processedFootprintData[["promoterBoundFlankSignal"]]
+  aggregateFootprintData[dataIdx,33] <- processedFootprintData[["promoterBoundBackgroundSignal"]]
+  aggregateFootprintData[dataIdx,34] <- processedFootprintData[["promoterBound.log2Flank"]]
+  aggregateFootprintData[dataIdx,35] <- processedFootprintData[["promoterBound.log2Depth"]]
+  aggregateFootprintData[dataIdx,36] <- processedFootprintData[["promoterUnboundMotifSignal"]]
+  aggregateFootprintData[dataIdx,37] <- processedFootprintData[["promoterUnboundFlankSignal"]]
+  aggregateFootprintData[dataIdx,38] <- processedFootprintData[["promoterUnboundBackgroundSignal"]]
+  aggregateFootprintData[dataIdx,39] <- processedFootprintData[["promoterUnbound.log2Flank"]]
+  aggregateFootprintData[dataIdx,40] <- processedFootprintData[["promoterUnbound.log2Depth"]]
+  aggregateFootprintData[dataIdx,41] <- processedFootprintData[["distalBoundMotifSignal"]]
+  aggregateFootprintData[dataIdx,42] <- processedFootprintData[["distalBoundFlankSignal"]]
+  aggregateFootprintData[dataIdx,43] <- processedFootprintData[["distalBoundBackgroundSignal"]]
+  aggregateFootprintData[dataIdx,44] <- processedFootprintData[["distalBound.log2Flank"]]
+  aggregateFootprintData[dataIdx,45] <- processedFootprintData[["distalBound.log2Depth"]]
+  aggregateFootprintData[dataIdx,46] <- processedFootprintData[["distalUnboundMotifSignal"]]
+  aggregateFootprintData[dataIdx,47] <- processedFootprintData[["distalUnboundFlankSignal"]]
+  aggregateFootprintData[dataIdx,48] <- processedFootprintData[["distalUnboundBackgroundSignal"]]
+  aggregateFootprintData[dataIdx,49] <- processedFootprintData[["distalUnbound.log2Flank"]]
+  aggregateFootprintData[dataIdx,50] <- processedFootprintData[["distalUnbound.log2Depth"]]
+  aggregateFootprintData[dataIdx,51] <- processedFootprintData[["numBoundSites"]] / processedFootprintData[["numPeakSites"]]
+
+ 
   ##
   tempList <- list()
   tempList$"peakSites" <- processedFootprintData[["peakSites"]]
