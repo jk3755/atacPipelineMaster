62665063b900eb79bfae4ea140c4edf856b94707
Jordan <jsk336@gmail.com>
2019-04-24 22:20:46 -0400

    Added a timeout function to the analyze raw fp script

--- panTF/snakeAnalyzeRawFootprint.R
+++ panTF/snakeAnalyzeRawFootprint.R
@@ -21,7 +21,7 @@ suppressMessages(library(parallel))
 suppressMessages(library(Rsamtools))
 suppressMessages(library(GenomicAlignments))
 suppressMessages(library(genomation))
-#suppressMessages(library(R.utils))
+suppressMessages(library(R.utils))
 
 ## Set snakemake variables
 cat("Setting snakemake variables...", "\n")
@@ -41,6 +41,9 @@ numMotif <- length(bindingSites)
 bamFile <- BamFile(bamPath)
 footprintDataPath <- paste0(dirPath, "footprints/data/raw/", sampleName, ".", geneName, ".rawFootprintData.Rdata")
 
+## Timeout path - file to be generated to indicate this thread timed out
+timeoutPath <- paste0(dirPath, "footprints/operations/raw/timeout", sampleName, ".", geneName, ".rawFootprintData.timeout")
+
 ## Peaks
 cat("Loading accessibility peaks...", "\n")
 grPeaks <- readBed(peakPath, track.line = FALSE, remove.unusual = FALSE, zero.based = TRUE)
@@ -48,6 +51,7 @@ grPeaks <- keepStandardChromosomes(grPeaks, pruning.mode="coarse")
 
 ## Initiate an R object to hold all generated data
 footprintData <- list()
+
 for (a in 1:numMotif){
   com <- paste0("footprintData$motif", a, " <- list()")
   eval(parse(text = com))} # end for (a in 1:numMotif)
@@ -55,280 +59,148 @@ for (a in 1:numMotif){
 cat("Analyzing footprints for", geneName, "\n")
 cat("Found", numMotif, "unique motifs", "\n")
 
-
-analyzeRawFootprint <- function(){
+##
+analyzeRawFootprint <- function(numMotif){
   
-  ## Initiate a temporary list object to store data, will be transferred to footprintData list
-  tempData <- list()
-  
-  cat("Processing binding sites", "\n")
-  
-  ## Binding Sites
-  cat("Subsetting binding sites based on accessibility peaks", "\n")
-  allSites <- bindingSites[[b]][["sites"]]
-  peakSites <- subsetByOverlaps(allSites, grPeaks)
-  numPeakSites <- length(peakSites)
-  cat("Found", numPeakSites, "motif binding sites in peak accessibility regions", "\n")
-  
-  if (numPeakSites == 0){
+  for (b in 1:numMotif){
     
-    next
+    ## Initiate a temporary list object to store data, will be transferred to footprintData list
+    tempData <- list()
+    cat("Processing binding sites", "\n")
     
-  } else {
+    ## Binding Sites
+    cat("Subsetting binding sites based on accessibility peaks", "\n")
+    allSites <- bindingSites[[b]][["sites"]]
+    peakSites <- subsetByOverlaps(allSites, grPeaks)
+    numPeakSites <- length(peakSites)
+    cat("Found", numPeakSites, "motif binding sites in peak accessibility regions", "\n")
     
-    ## Transfer the data
-    tempData$PWM <- bindingSites[[b]][["PWM"]]
-    tempData$peakSites <- peakSites
-    tempData$numPeakSites <- numPeakSites
-    tempData$motifWidth <- length(bindingSites[[b]][["PWM"]][1,])
-    
-    cat("Processing analysis window for each site", "\n")
-    ## extend each range +/- 250 bp from motif edges
-    tempData$extSites <- promoters(tempData$peakSites, upstream = 250, downstream = (250 + tempData$motifWidth), use.names=TRUE)
-    ## Read in the data from bam file for current ranges
-    param <- ScanBamParam(which = tempData$extSites)
-    ## Use GenomicAlignments package to read in bam file to GRanges, also very fast
-    ## Consider each read as a unique element (insertion), not paired end
-    cat("Loading relevant reads", "\n")
-    bamIn <- readGAlignments(bamFile, param = param)
-    ## Convert GAlignments to GRanges
-    cat("Converting reads to insertions", "\n")
-    grIn <- granges(bamIn)
-    ## Trim everything but standard chromosomes, trim out of bounds ranges
-    grIn <- keepStandardChromosomes(grIn, pruning.mode="coarse")
-    grIn <- trim(grIn)
-    ## Convert the reads to insertions with width = 1
-    grIn2 <- resize(grIn, width = 1)
-    ## Subset the Granges object into plus and minus strands for shifting
-    cat("Shifting insertions +4/-5 bp", "\n")
-    plusIdx <- which(strand(grIn2) == "+")
-    minusIdx <- which(strand(grIn2) == "-")
-    grPlus <- grIn2[plusIdx]
-    grMinus <- grIn2[minusIdx]
-    ## Shift the ATACseq reads to account for Tn5 insertion mechanism 
-    ## shift end of fragment +4 bp (plus strand) or -5 bp (minus standed)
-    grPlusShifted <- shift(grPlus, shift=4L)
-    grMinusShifted <- shift(grMinus, shift=-5L)
-    ## Merge the plus and minus strand shifted Granges
-    grMerged <- c(grPlusShifted, grMinusShifted)
-    tempData$shiftedInsertions <- grMerged
-    
-    ## Perform the footprint calculations
-    ## Convert Tn5 insertions corrected Granges to Rle object
-    cat("Generating insertion matrix", "\n")
-    insRLE <- coverage(grMerged)
-    ## Get the matching sites
-    extSites <- tempData$extSites
-    extSites <- keepStandardChromosomes(extSites, pruning.mode="coarse")
-    extSites <- trim(extSites)
-    ## Create a views object for the Rle list using the Granges sites data
-    insViews <- Views(insRLE, extSites)
-    ## Convert to a matrix
-    insMatrix <- as.matrix(insViews)
-    
-    ## Calculate the insertion probability at each basepair
-    cat("Calculating insertion probabilies", "\n")
-    rawTotalSignal <- sum(insMatrix)
-    rawProfile <- matrix(data = NA, ncol = length(insMatrix[1,]), nrow = 2)
-    rownames(rawProfile) <- c("Column sums", "Insertion frequency")
-    ##
-    for (c in 1:length(insMatrix[1,])){
-      rawProfile[1,c] <- sum(insMatrix[,c])
-      rawProfile[2,c] <- (rawProfile[1,c] / rawTotalSignal) * 100
-    } # end for (c in 1:length(insMatrix[1,]))
-    
-    ## Store the data
-    cat("Storing data", "\n")
-    tempData$extSites <- extSites
-    tempData$insRLE <- insRLE
-    tempData$insViews <- insViews
-    tempData$insMatrix <- insMatrix
-    tempData$rawTotalSignal <- rawTotalSignal
-    tempData$rawProfile <- rawProfile
-    tempData$libSize <- length(bamIn)
-    tempData$coverageSize <- sum(as.numeric(width(reduce(grIn, ignore.strand=TRUE))))
-    tempData$libFactor <- tempData$libSize / tempData$coverageSize
-    ##
-    rm(extSites, insRLE, insViews, insMatrix, rawTotalSignal, rawProfile, bamIn)
-    rm(grIn, grIn2, plusIdx, minusIdx, grPlus, grMinus, grPlusShifted, grMinusShifted)
-    gc()
-    
-    ## Calculate flanking accessibility and footprint depth data
-    cat("Calculating flanking accessibility and footprint depth data", "\n")
-    rawFootprintMetrics <- matrix(data = NA, ncol = 5, nrow = length(tempData$insMatrix[,1]))
-    colnames(rawFootprintMetrics) <- c("Background", "Flanking", "Motif", "Flanking Accessibility", "Footprint Depth")
-    
-    for (d in 1:length(tempData$insMatrix[,1])){
-      rawFootprintMetrics[d,1] <- (sum(tempData$insMatrix[d,1:50]) + sum(tempData$insMatrix[d,(450 + tempData$motifWidth):(500 + tempData$motifWidth)]))
-      rawFootprintMetrics[d,2] <- (sum(tempData$insMatrix[d,200:250]) + sum(tempData$insMatrix[d,(200 + tempData$motifWidth):(250 + tempData$motifWidth)]))
-      rawFootprintMetrics[d,3] <- sum(tempData$insMatrix[d,(250:(250 + tempData$motifWidth))])
-      rawFootprintMetrics[d,4] <- rawFootprintMetrics[d,2] / rawFootprintMetrics[d,1]
-      rawFootprintMetrics[d,5] <- rawFootprintMetrics[d,3] / rawFootprintMetrics[d,2]
-    } # end (for d in 1:length(tempData$insMatrix[,1]))
-    
-    tempData$rawFootprintMetrics <- rawFootprintMetrics
-    rm(rawFootprintMetrics)
-    gc()
-    
-    #### Transfer all the data for the current motif to the storage object
-    cat("Transferring all data to storage object footprintData", "\n")
-    com <- paste0("footprintData$motif", idxMotif, " <- tempData")
-    eval(parse(text = com))
-    
-    ## Update the motif index
-    idxMotif <- (idxMotif + 1)
-    
-  } # end if (numPeakSites = 0)
-}
+    if (numPeakSites == 0){
+      next
+    } else {
+      
+      ## Transfer the data
+      tempData$PWM <- bindingSites[[b]][["PWM"]]
+      tempData$peakSites <- peakSites
+      tempData$numPeakSites <- numPeakSites
+      tempData$motifWidth <- length(bindingSites[[b]][["PWM"]][1,])
+      cat("Processing analysis window for each site", "\n")
+      ## extend each range +/- 250 bp from motif edges
+      tempData$extSites <- promoters(tempData$peakSites, upstream = 250, downstream = (250 + tempData$motifWidth), use.names=TRUE)
+      ## Read in the data from bam file for current ranges
+      param <- ScanBamParam(which = tempData$extSites)
+      ## Use GenomicAlignments package to read in bam file to GRanges, also very fast
+      ## Consider each read as a unique element (insertion), not paired end
+      cat("Loading relevant reads", "\n")
+      bamIn <- readGAlignments(bamFile, param = param)
+      ## Convert GAlignments to GRanges
+      cat("Converting reads to insertions", "\n")
+      grIn <- granges(bamIn)
+      ## Trim everything but standard chromosomes, trim out of bounds ranges
+      grIn <- keepStandardChromosomes(grIn, pruning.mode="coarse")
+      grIn <- trim(grIn)
+      ## Convert the reads to insertions with width = 1
+      grIn2 <- resize(grIn, width = 1)
+      ## Subset the Granges object into plus and minus strands for shifting
+      cat("Shifting insertions +4/-5 bp", "\n")
+      plusIdx <- which(strand(grIn2) == "+")
+      minusIdx <- which(strand(grIn2) == "-")
+      grPlus <- grIn2[plusIdx]
+      grMinus <- grIn2[minusIdx]
+      ## Shift the ATACseq reads to account for Tn5 insertion mechanism 
+      ## shift end of fragment +4 bp (plus strand) or -5 bp (minus standed)
+      grPlusShifted <- shift(grPlus, shift=4L)
+      grMinusShifted <- shift(grMinus, shift=-5L)
+      ## Merge the plus and minus strand shifted Granges
+      grMerged <- c(grPlusShifted, grMinusShifted)
+      tempData$shiftedInsertions <- grMerged
+      ## Perform the footprint calculations
+      ## Convert Tn5 insertions corrected Granges to Rle object
+      cat("Generating insertion matrix", "\n")
+      insRLE <- coverage(grMerged)
+      ## Get the matching sites
+      extSites <- tempData$extSites
+      extSites <- keepStandardChromosomes(extSites, pruning.mode="coarse")
+      extSites <- trim(extSites)
+      ## Create a views object for the Rle list using the Granges sites data
+      insViews <- Views(insRLE, extSites)
+      ## Convert to a matrix
+      insMatrix <- as.matrix(insViews)
+      ## Calculate the insertion probability at each basepair
+      cat("Calculating insertion probabilies", "\n")
+      rawTotalSignal <- sum(insMatrix)
+      rawProfile <- matrix(data = NA, ncol = length(insMatrix[1,]), nrow = 2)
+      rownames(rawProfile) <- c("Column sums", "Insertion frequency")
+      
+      ##
+      for (c in 1:length(insMatrix[1,])){
+        rawProfile[1,c] <- sum(insMatrix[,c])
+        rawProfile[2,c] <- (rawProfile[1,c] / rawTotalSignal) * 100
+      } # end for (c in 1:length(insMatrix[1,]))
+      
+      ## Store the data
+      cat("Storing data", "\n")
+      tempData$extSites <- extSites
+      tempData$insRLE <- insRLE
+      tempData$insViews <- insViews
+      tempData$insMatrix <- insMatrix
+      tempData$rawTotalSignal <- rawTotalSignal
+      tempData$rawProfile <- rawProfile
+      tempData$libSize <- length(bamIn)
+      tempData$coverageSize <- sum(as.numeric(width(reduce(grIn, ignore.strand=TRUE))))
+      tempData$libFactor <- tempData$libSize / tempData$coverageSize
+      ##
+      rm(extSites, insRLE, insViews, insMatrix, rawTotalSignal, rawProfile, bamIn)
+      rm(grIn, grIn2, plusIdx, minusIdx, grPlus, grMinus, grPlusShifted, grMinusShifted)
+      gc()
+      
+      ## Calculate flanking accessibility and footprint depth data
+      cat("Calculating flanking accessibility and footprint depth data", "\n")
+      rawFootprintMetrics <- matrix(data = NA, ncol = 5, nrow = length(tempData$insMatrix[,1]))
+      colnames(rawFootprintMetrics) <- c("Background", "Flanking", "Motif", "Flanking Accessibility", "Footprint Depth")
+      
+      for (d in 1:length(tempData$insMatrix[,1])){
+        rawFootprintMetrics[d,1] <- (sum(tempData$insMatrix[d,1:50]) + sum(tempData$insMatrix[d,(450 + tempData$motifWidth):(500 + tempData$motifWidth)]))
+        rawFootprintMetrics[d,2] <- (sum(tempData$insMatrix[d,200:250]) + sum(tempData$insMatrix[d,(200 + tempData$motifWidth):(250 + tempData$motifWidth)]))
+        rawFootprintMetrics[d,3] <- sum(tempData$insMatrix[d,(250:(250 + tempData$motifWidth))])
+        rawFootprintMetrics[d,4] <- rawFootprintMetrics[d,2] / rawFootprintMetrics[d,1]
+        rawFootprintMetrics[d,5] <- rawFootprintMetrics[d,3] / rawFootprintMetrics[d,2]
+      } # end (for d in 1:length(tempData$insMatrix[,1]))
+      
+      tempData$rawFootprintMetrics <- rawFootprintMetrics
+      rm(rawFootprintMetrics)
+      gc()
+      
+      #### Transfer all the data for the current motif to the storage object
+      cat("Transferring all data to storage object footprintData", "\n")
+      com <- paste0("footprintData$motif", idxMotif, " <<- tempData")
+      eval(parse(text = com))
+      
+      ## Update the motif index
+      idxMotif <- (idxMotif + 1)
+      
+    } # end if (numPeakSites = 0)
+  } # end for (b in 1:numMotif)
+} # end analyzeRawFootprint function
 
-
-
-## Index counter for motif naming, required in case some motifs have no matches in peak sites
-idxMotif <- 1
 
 ## Begin analysis
-for (b in 1:numMotif){
-  
-  
-  
-  ## Initiate a temporary list object to store data, will be transferred to footprintData list
-  tempData <- list()
-  
-  cat("Processing binding sites", "\n")
-  
-  ## Binding Sites
-  cat("Subsetting binding sites based on accessibility peaks", "\n")
-  allSites <- bindingSites[[b]][["sites"]]
-  peakSites <- subsetByOverlaps(allSites, grPeaks)
-  numPeakSites <- length(peakSites)
-  cat("Found", numPeakSites, "motif binding sites in peak accessibility regions", "\n")
-  
-  if (numPeakSites == 0){
-    
-    next
-    
-  } else {
-  
-  ## Transfer the data
-  tempData$PWM <- bindingSites[[b]][["PWM"]]
-  tempData$peakSites <- peakSites
-  tempData$numPeakSites <- numPeakSites
-  tempData$motifWidth <- length(bindingSites[[b]][["PWM"]][1,])
-  
-  cat("Processing analysis window for each site", "\n")
-  ## extend each range +/- 250 bp from motif edges
-  tempData$extSites <- promoters(tempData$peakSites, upstream = 250, downstream = (250 + tempData$motifWidth), use.names=TRUE)
-  ## Read in the data from bam file for current ranges
-  param <- ScanBamParam(which = tempData$extSites)
-  ## Use GenomicAlignments package to read in bam file to GRanges, also very fast
-  ## Consider each read as a unique element (insertion), not paired end
-  cat("Loading relevant reads", "\n")
-  bamIn <- readGAlignments(bamFile, param = param)
-  ## Convert GAlignments to GRanges
-  cat("Converting reads to insertions", "\n")
-  grIn <- granges(bamIn)
-  ## Trim everything but standard chromosomes, trim out of bounds ranges
-  grIn <- keepStandardChromosomes(grIn, pruning.mode="coarse")
-  grIn <- trim(grIn)
-  ## Convert the reads to insertions with width = 1
-  grIn2 <- resize(grIn, width = 1)
-  ## Subset the Granges object into plus and minus strands for shifting
-  cat("Shifting insertions +4/-5 bp", "\n")
-  plusIdx <- which(strand(grIn2) == "+")
-  minusIdx <- which(strand(grIn2) == "-")
-  grPlus <- grIn2[plusIdx]
-  grMinus <- grIn2[minusIdx]
-  ## Shift the ATACseq reads to account for Tn5 insertion mechanism 
-  ## shift end of fragment +4 bp (plus strand) or -5 bp (minus standed)
-  grPlusShifted <- shift(grPlus, shift=4L)
-  grMinusShifted <- shift(grMinus, shift=-5L)
-  ## Merge the plus and minus strand shifted Granges
-  grMerged <- c(grPlusShifted, grMinusShifted)
-  tempData$shiftedInsertions <- grMerged
-  
-  ## Perform the footprint calculations
-  ## Convert Tn5 insertions corrected Granges to Rle object
-  cat("Generating insertion matrix", "\n")
-  insRLE <- coverage(grMerged)
-  ## Get the matching sites
-  extSites <- tempData$extSites
-  extSites <- keepStandardChromosomes(extSites, pruning.mode="coarse")
-  extSites <- trim(extSites)
-  ## Create a views object for the Rle list using the Granges sites data
-  insViews <- Views(insRLE, extSites)
-  ## Convert to a matrix
-  insMatrix <- as.matrix(insViews)
-  
-  ## Calculate the insertion probability at each basepair
-  cat("Calculating insertion probabilies", "\n")
-  rawTotalSignal <- sum(insMatrix)
-  rawProfile <- matrix(data = NA, ncol = length(insMatrix[1,]), nrow = 2)
-  rownames(rawProfile) <- c("Column sums", "Insertion frequency")
-  ##
-  for (c in 1:length(insMatrix[1,])){
-    rawProfile[1,c] <- sum(insMatrix[,c])
-    rawProfile[2,c] <- (rawProfile[1,c] / rawTotalSignal) * 100
-  } # end for (c in 1:length(insMatrix[1,]))
-  
-  ## Store the data
-  cat("Storing data", "\n")
-  tempData$extSites <- extSites
-  tempData$insRLE <- insRLE
-  tempData$insViews <- insViews
-  tempData$insMatrix <- insMatrix
-  tempData$rawTotalSignal <- rawTotalSignal
-  tempData$rawProfile <- rawProfile
-  tempData$libSize <- length(bamIn)
-  tempData$coverageSize <- sum(as.numeric(width(reduce(grIn, ignore.strand=TRUE))))
-  tempData$libFactor <- tempData$libSize / tempData$coverageSize
-  ##
-  rm(extSites, insRLE, insViews, insMatrix, rawTotalSignal, rawProfile, bamIn)
-  rm(grIn, grIn2, plusIdx, minusIdx, grPlus, grMinus, grPlusShifted, grMinusShifted)
-  gc()
-  
-  ## Calculate flanking accessibility and footprint depth data
-  cat("Calculating flanking accessibility and footprint depth data", "\n")
-  rawFootprintMetrics <- matrix(data = NA, ncol = 5, nrow = length(tempData$insMatrix[,1]))
-  colnames(rawFootprintMetrics) <- c("Background", "Flanking", "Motif", "Flanking Accessibility", "Footprint Depth")
-  
-  for (d in 1:length(tempData$insMatrix[,1])){
-    rawFootprintMetrics[d,1] <- (sum(tempData$insMatrix[d,1:50]) + sum(tempData$insMatrix[d,(450 + tempData$motifWidth):(500 + tempData$motifWidth)]))
-    rawFootprintMetrics[d,2] <- (sum(tempData$insMatrix[d,200:250]) + sum(tempData$insMatrix[d,(200 + tempData$motifWidth):(250 + tempData$motifWidth)]))
-    rawFootprintMetrics[d,3] <- sum(tempData$insMatrix[d,(250:(250 + tempData$motifWidth))])
-    rawFootprintMetrics[d,4] <- rawFootprintMetrics[d,2] / rawFootprintMetrics[d,1]
-    rawFootprintMetrics[d,5] <- rawFootprintMetrics[d,3] / rawFootprintMetrics[d,2]
-  } # end (for d in 1:length(tempData$insMatrix[,1]))
-  
-  tempData$rawFootprintMetrics <- rawFootprintMetrics
-  rm(rawFootprintMetrics)
-  gc()
-  
-  #### Transfer all the data for the current motif to the storage object
-  cat("Transferring all data to storage object footprintData", "\n")
-  com <- paste0("footprintData$motif", idxMotif, " <- tempData")
-  eval(parse(text = com))
-  
-  ## Update the motif index
-  idxMotif <- (idxMotif + 1)
-  
-  } # end if (numPeakSites = 0)
-} # end for (b in 1:numMotif)
+
+tryCatch(
+  expr = {
+    withTimeout(analyzeRawFootprint(numMotif),
+                timeout = 10)
+  },
+  TimeoutException = function(ex) {cat("Timeout. Skipping for now\n"); file.create(timeoutPath)}
+)
 
 ## Save the raw footprint data
 cat("Saving finished data for", geneName, "\n")
-save(footprintData, file = footprintDataPath)
+
+tryCatch(save(footprintData, file = footprintDataPath))
 
 ##
 file.create(outPath)
 
 
 
-## Required for the timeout exception
-#tryCatch(
-#  expr = {
-#    withTimeout({cat("Analyzing motif", b, "\n")
-#    )},
-#    timeout = 600)
-#  },
-#  TimeoutException = function(ex) cat("Script took too long, skipping for now\n")
-#  )}
