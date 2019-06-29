6ad0abe1f7c754a616d00acab3575d47b6397976
Jordan <jsk336@gmail.com>
2019-06-29 07:27:02 -0400

    Cluster updates

--- qsubSubmit.sh
+++ qsubSubmit.sh
@@ -11,5 +11,5 @@ module load conda
 echo "source activate atac"
 source activate atac
 #
-echo "snakemake --snakefile snakefileATACseqWorkflow.snakefile -j 100 rawFP_lncap_ex01 --cluster-config snakeResources/cluster/qsubConfig.json --cluster 'qsub -cwd -pe smp {cluster.nCPUs} -l mem={cluster.memory}M,time=8:0:0'-V -verbose"
-snakemake --snakefile snakefileATACseqWorkflow.snakefile -j 100 rawFP_lncap_ex01 --cluster-config snakeResources/cluster/qsubConfig.json --cluster "qsub -cwd -pe smp {cluster.nCPUs} -l mem={cluster.memory}M,time=8:0:0 -V -verbose"
+echo "snakemake --snakefile snakefileATACseqWorkflow.snakefile -j 100 rawFP_lncap_ex01 --cluster-config snakeResources/cluster/qsubConfig.json --cluster 'qsub -cwd -pe smp {cluster.nCPUs} -l mem={cluster.memory}M,time=8:0:0'-V" --latency-wait 60
+snakemake --snakefile snakefileATACseqWorkflow.snakefile -j 100 rawFP_lncap_ex01 --cluster-config snakeResources/cluster/qsubConfig.json --cluster "qsub -cwd -pe smp {cluster.nCPUs} -l mem={cluster.memory}M,time=8:0:0 -V -e {cluster.error} -o {cluster.output}" --latency-wait 60
