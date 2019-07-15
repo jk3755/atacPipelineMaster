4836f7e6b50f5709d31d44b53edc252a7fd0cb7f
Jordan <jsk336@gmail.com>
2019-07-15 03:37:05 -0400

    Chipseeker code working, not integrated with pipeline

--- snakeResources/scripts/footprints/snakeaAnnotateFootprints.R
+++ snakeResources/scripts/footprints/snakeaAnnotateFootprints.R
@@ -14,6 +14,7 @@ library(org.Hs.eg.db)
 library(ggplot2)
 library(clusterProfiler)
 library(ReactomePA)
+library(org.Hs.eg.db)
 
 
 ##
@@ -24,19 +25,31 @@ x <- annotatePeak(
                   peak = test,
                   tssRegion = c(-3000, 3000),
                   TxDb = TxDb.Hsapiens.UCSC.hg38.knownGene,
-                  level = "transcript",
+                  level = "gene",
                   assignGenomicAnnotation = TRUE,
                   genomicAnnotationPriority = c("Promoter", "5UTR", "3UTR", "Exon", "Intron", "Downstream", "Intergenic"),
-                  annoDb = NULL,
+                  annoDb = "org.Hs.eg.db",
                   addFlankGeneInfo = FALSE,
                   flankDistance = 5000,
                   sameStrand = FALSE,
                   ignoreOverlap = FALSE,
-                  ignoreUpstream = FALSE,ignoreDownstream = FALSE,
+                  ignoreUpstream = FALSE,
+                  ignoreDownstream = FALSE,
                   overlap = "TSS",
                   verbose = TRUE)
 
+### Output (temp) ###
+outpath <- "C:\\Users\\jsk33\\Desktop\\ARgenes.txt"
 
+##
+write.table(
+  genes,
+  file = outpath,
+  quote = FALSE,
+  sep = ",",
+  eol = "\n",
+  row.names = FALSE,
+  col.names = FALSE)
 
 
 
